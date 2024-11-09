a492a527a1b3eb44f38135b4786343fd6006c9b8
rapolu prashanth <rapoluprashanth123@gmail.com>
2024-11-09 13:16:56 +0530

    add assignment5

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About:React.FC = () => {
+  return (
+    <div>
+        <h1>About(using Functional Components)</h1>
+        <p>This application provides information about the products..</p>
+    </div>
+  )
+}
+
+export default About
